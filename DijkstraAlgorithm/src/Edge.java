3848005fd9cbb5871b72378c9b68fad20e8c0b2e
IvanSteven <IvanSteven@192.168.1.232>
2013-12-13 01:35:34 +0000

    First commit.
    Signed-off-by: IvanSteven <IvanSteven@192.168.1.232>

---
+++ DijkstraAlgorithm/src/Edge.java
@@ -0,0 +1,7 @@
+class Edge
+{
+    public final Vertex target;
+    public final double weight;
+    public Edge(Vertex argTarget, double argWeight)
+    { target = argTarget; weight = argWeight; }
+}
