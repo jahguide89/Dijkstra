dc02b40959a980c88c032aba7bb3b8e4dc20d4aa
IvanSteven <IvanSteven@192.168.1.232>
2013-12-17 23:13:59 +0000

    Actualizacion del algoritmo con sus repsectivas pruebas
    Signed-off-by: IvanSteven <IvanSteven@192.168.1.232>

--- DijkstraAlgorithm/src/Edge.java
+++
@@ -1,7 +0,0 @@
-class Edge
-{
-    public final Vertex target;
-    public final double weight;
-    public Edge(Vertex argTarget, double argWeight)
-    { target = argTarget; weight = argWeight; }
-}
